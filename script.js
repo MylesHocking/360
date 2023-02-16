3d4da6b8b11ad2f46f2c443c942b6c79987892d9
MylesHocking <myleshocking@gmail.com>
2023-02-16 09:54:41 +0000

    latest

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-var links = [{label: '1', bg: '#c0392b', backgroundImage = "url('img_tree.png')";}, 
+var links = [{label: '1', bg: '#c0392b'}, 
              {label: '2', bg: '#16a085'}, 
              {label: '3', bg: '#8e44ad'}, 
              {label: '4', bg: '#27ae60'}, 
              {label: '5', bg: '#f39c12'}, 
-             {label: '6', bg: '#2980b9'}];
+             {label: '6', bg: '#2980b9'},
+	     {label: '11', bg: '#c0392b'}, 
+             {label: '12', bg: '#16a085'}, 
+             {label: '13', bg: '#8e44ad'}, 
+             {label: '14', bg: '#27ae60'}, 
+             {label: '15', bg: '#f39c12'}, 
+             {label: '16', bg: '#2980b9'}];
 var windowHeight = 720; //= //window.innerHeight;
 if(windowHeight === 0) windowHeight = 720;
 var //radius = 720,//windowHeight*0.2,
-    radius = windowHeight*0.16,
+    radius = windowHeight,//*0.16,
     circle = document.createElement('div'),
-    borderSize = radius*0.021,
-    totalArea = 36, 
+    borderSize = radius*0.011,
+    totalArea = 72, 
     increment = totalArea/(links.length-1),
-    startPoint = 0+(totalArea/2),
+    startPoint = -10+(totalArea/2),
     fontSize = radius*0.12,
     linkSize = radius*0.25;
+
+circle2 = document.createElement('div');
+circle2.innerHTML = "This is here";
+
+// Create element:
+const para = document.createElement("p");
+para.innerHTML = "This is a paragraph.";
+
+// Append to another element:
+document.getElementById("myDIV").appendChild(para);
+
+
+alert("WH"+windowHeight+"radius"+radius+"borderSize"+borderSize+"increment"+increment+"startPoint"+startPoint);
 
 styleCircle();
 addCircle();
@@ -131,3 +150,4 @@ function setTransition(element, string) {
   element.style.OTransition = string;
   element.style.transition = string;
 }
+
